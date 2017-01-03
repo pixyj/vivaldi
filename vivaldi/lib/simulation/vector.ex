22347ff89b02dc5b5cca66498687e30a62a8a077
Pramod Lakshmanan <pramodliv1@gmail.com>
2017-01-03 23:43:24 +0530

    Centralized Algo: Add more vector functions.

--- vivaldi/lib/simulation/vector.ex
+++ vivaldi/lib/simulation/vector.ex
@@ -1,15 +1,37 @@
 defmodule Vivaldi.Simulation.Vector do
-
-  @doc """
-    Calculates vector distance.
-    Assumes `x_i` and `x_j` dimensions match. 
+  @moduledoc """
+  Vector functions. All functions assume dimensions match.
   """
-  def distance(x_i, x_j) do
-    Enum.zip(x_i, x_j)
-    |> Enum.map(fn {a, b} -> a - b end)
+
+  def distance(p1, p2) do
+    diff(p1, p2)
+    |> magnitude()
+  end
+
+  def diff(p1, p2) do
+    Enum.zip(p1, p2)
+    |> Enum.map(fn {a, b} -> (a - b) end)
+  end
+
+  def magnitude(p1) do
+    p1
     |> Enum.map(&(&1 * &1))
     |> Enum.reduce(&(&1 + &2))
     |> :math.sqrt()
   end
-  
+
+  def scale(p1, factor) do
+    Enum.map(p1, fn a -> a * factor end)
+  end
+
+  def unit_vector_at(p1, p2) do
+    # Return zero vector if p1 == p2
+    if p1 == p2 do
+      Enum.map(1..(Enum.count(p1)), fn _ -> 0 end)
+    else
+      d = diff(p1, p2)
+      mag = magnitude(d)
+      scale(d, 1 / mag)
+    end
+  end
 end
--- vivaldi/test/vector_test.exs
+++ vivaldi/test/vector_test.exs
@@ -17,4 +17,58 @@ defmodule VectorTest do
       assert abs(result - expected_distance) < 0.00001
     end)
   end
+
+  test "Vector Diff" do
+    points_and_results = [
+     {[0, 0], [0, 0], [0, 0]},
+     {[0, 1], [0, 0], [0, 1]},
+     {[2, 1], [1, 2], [1, -1]},
+     {[0, 3], [4, 0], [-4, 3]},
+    ]
+
+    Enum.each(points_and_results, fn {p1, p2, expected} ->
+      result = Vector.diff(p1, p2)
+      assert expected == result
+    end)
+  end
+
+  test "Vector Magnitude" do
+    points_and_results = [
+     {[0, 0], 0},
+     {[0, 3, 4], 5},
+     {[1, 1], :math.sqrt(2)},
+     {[-1, -1], :math.sqrt(2)},
+    ]
+
+    Enum.each(points_and_results, fn {p1, expected} ->
+      result = Vector.magnitude(p1)
+      assert expected == result
+    end)
+  end
+
+  test "Vector Scale" do
+    inputs_and_results = [
+     {[0, 0], 100, [0, 0]},
+     {[0, 3, 4], 2, [0, 6, 8]},
+     {[1, 1], -4, [-4, -4]},
+     {[-1, -1], -4, [4, 4]},
+    ]
+
+    Enum.each(inputs_and_results, fn {p1, factor, expected} ->
+      result = Vector.scale(p1, factor)
+      assert expected == result
+    end)
+  end
+
+  test "Unit vector at" do
+    inputs_and_results = [
+     {[1, 1], [0, 0], [:math.sin(:math.pi/4), :math.sin(:math.pi/4)]},
+     {[0, 0, 0], [0, 0, 0], [0, 0, 0]}
+    ]
+
+    Enum.each(inputs_and_results, fn {p1, p2, expected} ->
+      result = Vector.unit_vector_at(p1, p2)
+      assert expected == result
+    end)
+  end
 end
