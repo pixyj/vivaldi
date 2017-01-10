d69beac04ba1d8b5c6cfa4f3cfa07a5ffe6c9703
Pramod Lakshmanan <pramodliv1@gmail.com>
2017-01-10 23:29:06 +0530

    Visualize coordinate update events on a 3d scatter plot.
    HighCharts is awesome!

---
+++ visualization/index.js
@@ -0,0 +1,135 @@
+function getInitialPositions() {
+    return events.splice(0, 10).map(function(event) {
+        var point = event['x_i']; 
+        point.push(0); //2d to 3d; 
+        return point; 
+    });
+}
+
+function run() {
+    var updateEvents = events.splice(10); 
+    for (var i = 0, length = updateEvents.length; i < length; i++) {
+        (function(i) {
+            var e = updateEvents[i];
+            setTimeout(function() {
+                var x_i = e['x_i']
+                console.log("updating ", x_i); 
+                chart.series[0].data[e.i].update({
+                    x: x_i[0],
+                    y: x_i[1]
+                });
+                //chart.redraw(); 
+            }, i * 200); 
+        })(i); 
+    }
+}
+
+$(function () {
+
+    // Give the points a 3D feel by adding a radial gradient
+    Highcharts.getOptions().colors = $.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.4,
+                cy: 0.3,
+                r: 0.5
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.2).get('rgb')]
+            ]
+        };
+    });
+
+    // Set up the chart
+    var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'container',
+            margin: 100,
+            type: 'scatter',
+            options3d: {
+                enabled: true,
+                alpha: 10,
+                beta: 30,
+                depth: 250,
+                viewDistance: 5,
+                fitToPlot: false,
+                frame: {
+                    bottom: { size: 1, color: 'rgba(0,0,0,0.02)' },
+                    back: { size: 1, color: 'rgba(0,0,0,0.04)' },
+                    side: { size: 1, color: 'rgba(0,0,0,0.06)' }
+                }
+            }
+        },
+        title: {
+            text: 'Draggable box'
+        },
+        subtitle: {
+            text: 'Click and drag the plot area to rotate in space'
+        },
+        plotOptions: {
+            scatter: {
+                width: 10,
+                height: 10,
+                depth: 10
+            }
+        },
+        yAxis: {
+            min: -6,
+            max: 6,
+            title: null
+        },
+        xAxis: {
+            min: -6,
+            max: 6,
+            gridLineWidth: 1
+        },
+        zAxis: {
+            min: 0,
+            max: 10,
+            showFirstLabel: false
+        },
+        legend: {
+            enabled: false
+        },
+        series: [{
+            name: 'Reading',
+            colorByPoint: true,
+            data: getInitialPositions()
+        }]
+    });
+
+
+    // Add mouse events for rotation
+    $(chart.container).on('mousedown.hc touchstart.hc', function (eStart) {
+        eStart = chart.pointer.normalize(eStart);
+
+        var posX = eStart.pageX,
+            posY = eStart.pageY,
+            alpha = chart.options.chart.options3d.alpha,
+            beta = chart.options.chart.options3d.beta,
+            newAlpha,
+            newBeta,
+            sensitivity = 5; // lower is more sensitive
+
+        $(document).on({
+            'mousemove.hc touchdrag.hc': function (e) {
+                // Run beta
+                newBeta = beta + (posX - e.pageX) / sensitivity;
+                chart.options.chart.options3d.beta = newBeta;
+
+                // Run alpha
+                newAlpha = alpha + (e.pageY - posY) / sensitivity;
+                chart.options.chart.options3d.alpha = newAlpha;
+
+                chart.redraw(false);
+            },
+            'mouseup touchend': function () {
+                $(document).off('.hc');
+            }
+        });
+    });
+
+    window.chart = chart; 
+
+});
