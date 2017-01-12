d3792e6753e1088f941e5642ed1a81424f03cbed
Pramod Lakshmanan <pramodliv1@gmail.com>
2017-01-12 08:47:45 +0530

    Add helper scripts to test Connections

---
+++ vivaldi/test/peer_b.exs
@@ -0,0 +1,8 @@
+Node.start :"b@127.0.0.1"
+Node.set_cookie :"hide_and_seek"
+
+:global.register_name(:"b-ping-server", self)
+
+:timer.sleep(10000 * 1000)
+
+
---
+++ vivaldi/test/peer_c.exs
@@ -0,0 +1,6 @@
+Node.start :"c@127.0.0.1"
+Node.set_cookie :"hide_and_seek"
+
+:global.register_name(:"c-ping-server", self)
+
+:timer.sleep(2500)
