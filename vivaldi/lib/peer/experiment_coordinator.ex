eab47efad9b10e05566cd7009ee498f82237ec34
Pramod Lakshmanan <pramodliv1@gmail.com>
2017-01-14 06:03:28 +0530

    Experiment Coordinator: Add configure command. Remove "luxury" features. Fix minor semantic errors.

--- vivaldi/lib/peer/experiment_coordinator.ex
+++ vivaldi/lib/peer/experiment_coordinator.ex
@@ -12,36 +12,52 @@ defmodule Vivaldi.Peer.ExperimentCoordinator do
 
   require Logger
 
-  alias Vivaldi.Peer.{Connections, Coordinate, CoordinateLogger, 
+  alias Vivaldi.Peer.{AlgorithmSupervisor, Connections, Coordinate, CoordinateLogger, 
                       CoordinateStash, PingClient, PingServer}
 
-  def start_link(config, state_agent, algo_sup) do
-    name = get_name(config[:node_id])
-    GenServer.start_link(__MODULE__, [config, state_agent, algo_sup], name: name)
+  def start_link(node_id, state_agent) do
+    name = get_name(node_id)
+    GenServer.start_link(__MODULE__, [node_id, state_agent], name: name)
   end
 
   def get_name(node_id) do
     :"#{node_id}-experiment-coordinator"
   end
 
-  def init(config, state_agent) do
-    node_id = config[:node_id]
+  def init(node_id, state_agent) do
     name = get_name(node_id)
     Logger.info "#{node_id} - starting #{name}..."
-    status = Agent.get(state_agent, fn status -> status end)
+    status = Agent.get(state_agent, fn {status, _} -> status end)
     :yes = :global.register_name(name, self)
-    {:ok, {status, state_agent, config}}
+    {:ok, {status, state_agent, node_id}}
   end
 
-  def handle_call(:get_ready, _, {:just_started, state_agent, config}) do
-    node_id = config[:node_id]
+  def handle_info({:EXIT, pid, reason}, {:pinging, state_agent, node_id}) do
+    IO.puts "AlgorithmSupervisor,  #{pid} crashed #{inspect reason}. Restarting..."
+    config = Agent.get(state_agent, fn {_, config} -> config end)
+    spawn_algo_sup(config)
+    {:noreply, {:pinging, state_agent, node_id}}
+  end
+
+  def handle_call({:configure, config}, _, {:not_started, state_agent, node_id}) do
+    log_command_received(node_id, :configure, :not_started)
+    Agent.update(state_agent, fn {status, _} -> {status, config} end)
+
+    log_command_executed(node_id, :configure, :not_started)
+
+    next_status = :just_started
+    set_status(node_id, state_agent, :not_started, next_status)
+    {:reply, :ok, {next_status, state_agent, node_id}}
+  end
+
+  def handle_call(:get_ready, _, {:just_started, state_agent, node_id}) do
     names = [
       Connections.get_name(node_id),
-      Coordinate.get_name(node_id)
-      CoordinateLogger.get_name(node_id)
-      CoordinateStash.get_name(node_id)
-      PingClient.get_name(node_id)
-      PingServer.get_name(node_id)
+      Coordinate.get_name(node_id),
+      CoordinateLogger.get_name(node_id),
+      CoordinateStash.get_name(node_id),
+      PingClient.get_name(node_id),
+      PingServer.get_name(node_id),
     ]
 
     ready? = Enum.map(names, fn name -> 
@@ -56,92 +72,54 @@ defmodule Vivaldi.Peer.ExperimentCoordinator do
     |> Enum.filter(fn pid ->
       pid == nil
     end)
-    |> (fn nil_statuses -> Enum.count(nil_statuses) == 0).()
+    |> (fn nil_statuses -> Enum.count(nil_statuses) == 0 end).()
 
     if ready? do
-      {:reply, :ok, {:ready, state_agent, config}}
+      {:reply, :ok, {:ready, state_agent, node_id}}
     else
-      {:reply, :error, {:just_started, state_agent, config}}
+      {:reply, :error, {:just_started, state_agent, node_id}}
     end
   end
 
-  def handle_call(:begin_pings, _, {:ready, state_agent, config}) do
-    node_id = config[:node_id]
+  def handle_call(:begin_pings, _, {:ready, state_agent, node_id}) do
     name = PingClient.get_name(node_id)
-    case log_command_and_get_process(node_id, :begin_pings, name, {:ready, state_agent, config}) do
+    case log_command_and_get_process(node_id, :begin_pings, name, {:ready, state_agent, node_id}) do
       {:error, response} ->
         response
       {:ok, _} ->
         PingClient.begin_pings(node_id)
-        log_command_executed(node_id, :begin_pings, status)
+        log_command_executed(node_id, :begin_pings, :ready)
 
         next_status = :pinging
-        set_status(node_id, state_agent, status, next_status)
-        {:reply, :ok, {next_status, state_agent, config}}
+        set_status(node_id, state_agent, :ready, next_status)
+        {:reply, :ok, {next_status, state_agent, node_id}}
     end
 
-    node_id = config[:node_id]
     name = PingClient.get_name(node_id)
     case Process.whereis(name) do
       nil ->
         message = "#{node_id}: cannot execute command :begin_pings. PingClient isn't running"
         Logger.error message
-        {:reply, {:error, message}, {:just_started, state_agent, config}}
-      pid ->
-        {:reply, :ok, {:pinging, state_agent, config}}
-    end
-  end
-
-  def handle_call(:stop_pings, _, {:pinging, state_agent, config}) do
-    node_id = config[:node_id]
-    name = PingClient.get_name(node_id)
-    case log_command_and_get_process(node_id, :stop_pings, name, {:pinging, state_agent, config}) do
-      {:error, response} ->
-        response
-      {:ok, ping_client} ->
-        Process.exit ping_client, :kill
-        log_command_executed(node_id, :stop_pings, status)
-        :timer.sleep(100)
-        # Hopefully, ping_client is restarted by its supervisor by now
-        case Process.whereis(name) do
-          nil ->
-            Logger.error "#{node_id} -> ping_client has not restarted yet..."
-            next_status = :just_started
-            set_status(node_id, state_agent, status, next_status)
-            {:reply, {:error, :restart_failed}, {next_status, state_agent, config}}
-          _ ->
-            next_status = :ready
-            set_status(node_id, state_agent, status, next_status)
-            {:reply, :ok, {next_status, state_agent, config}}
-        end
-    end
-  end
-
-  def handle_call(:force_reset, _, {status, state_agent, config}) do
-    node_id = config[:node_id]
-    name = AlgorithmSupervisor.get_name(node_id)
-    case log_command_and_get_algo_sup(node_id, :force_reset, name, {status, state_agent, config}) do
-      {:error, response} ->
-        response
-      {:ok, algo_sup} ->
-        Process.exit pid, :kill
-        log_command_executed(node_id, :force_reset, status)
-        # Should we check if processes are restarted here? 
-        next_status = :just_started
-        set_status(node_id, state_agent, status, next_status)
-        {:reply, :ok, {next_status, state_agent, config}}
+        {:reply, {:error, message}, {:just_started, state_agent, node_id}}
+      _pid ->
+        {:reply, :ok, {:pinging, state_agent, node_id}}
     end
   end
 
   # TODO: Handle invalid commands
 
-  def log_command_and_get_process(node_id, command, name, {status, state_agent, config}) do
+  def spawn_algo_sup(config) do
+    Process.flag :exit, true
+    spawn_link(fn -> AlgorithmSupervisor.start_link(config) end)
+  end
+
+  def log_command_and_get_process(node_id, command, name, {status, state_agent, node_id}) do
     log_command_received(node_id, command, status)
     case Process.whereis(name) do
       nil ->
         message = "#{node_id} - ignored #{command}: #{name} isn't running"
         Logger.error message
-        {:error, {:reply, {:error, message}, {status, state_agent, config}}}
+        {:error, {:reply, {:error, message}, {status, state_agent, node_id}}}
       pid ->
         {:ok, pid}
     end
@@ -149,7 +127,7 @@ defmodule Vivaldi.Peer.ExperimentCoordinator do
 
   def set_status(node_id, state_agent, status, next_status) do
     if status != next_status do
-      Agent.update(state_agent, fn _ -> next_status end)
+      Agent.update(state_agent, fn {_, config} -> {next_status, config} end)
       Logger.info "#{node_id} - changed status #{status} -> #{next_status}"
     end
   end
