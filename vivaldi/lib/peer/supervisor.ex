876cb5a1b32c3daf2b1c6f45703de63d8482e1da
Pramod Lakshmanan <pramodliv1@gmail.com>
2017-01-14 06:02:33 +0530

    Add top-level peer supervisor.

---
+++ vivaldi/lib/peer/supervisor.ex
@@ -0,0 +1,25 @@
+defmodule Vivaldi.Peer.Supervisor do
+  @moduledoc """
+  Supervises all Vivaldi algorithm related processes, i.e. doesn't care about experiment setup
+  Scaffolding copied from https://hexdocs.pm/elixir/Supervisor.html
+  """
+
+  use Supervisor
+
+  alias Vivaldi.Peer.ExperimentCoordinator
+
+  def start_link(node_id) do
+    Supervisor.start_link(__MODULE__, [node_id])
+  end
+
+  def init([node_id]) do
+    state_agent = Agent.start_link fn -> {:not_started, nil} end, []
+    children = [
+      worker(ExperimentCoordinator, [node_id, state_agent]),
+    ]
+    supervise(children, strategy: :one_for_one)
+  end
+
+end
+
+
