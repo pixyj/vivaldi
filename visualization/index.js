ae8411809f2650f11eff80590c4104aef57c4635
Pramod Lakshmanan <pramodliv1@gmail.com>
2017-02-02 16:08:44 +0530

    Visualize algorithm for a cluster having 120 stable nodes and 4 new nodes.

--- visualization/index.js
+++ visualization/index.js
@@ -1,38 +1,192 @@
-function getInitialPositions() {
-    return events.splice(0, 10).map(function(event) {
-        var point = event['x_i']; 
-        point.push(0); //2d to 3d; 
-        return point; 
-    });
-}
-
-function run() {
-    var updateEvents = events.splice(10); 
-    for (var i = 0, length = updateEvents.length; i < length; i++) {
-        (function(i) {
-            var e = updateEvents[i];
-            setTimeout(function() {
-                var x_i = e['x_i']
-                console.log("updating ", x_i); 
-                chart.series[0].data[e.i].update({
-                    x: x_i[0],
-                    y: x_i[1]
-                });
-                //chart.redraw(); 
-            }, i * 50); 
-        })(i); 
-    }
-}
-
 $(function () {
+
+    var Animation = function(data) {
+        this.isPaused = false;
+        this.data = data;
+        this.nextEventIndex = 0;
+        this.length = this.data.events.length;
+
+        this.initializeTable();
+
+        this.$play = $("#event-play");
+        this.$pause = $("#event-pause");
+        this.$replay = $("#event-replay");
+
+        this.registerClickEvents();
+
+        this.$pause.hide();
+        this.$replay.hide();
+    };
+
+    Animation.prototype = {
+        
+        start: function() {
+            this.isPaused = false;
+            this.$play.hide();
+            this.$pause.show();
+            this.animateUpcomingEvents();
+        },
+
+        pause: function() {
+            this.isPaused = true;
+            this.$pause.hide();
+            this.$play.show();
+        },
+
+        replay: function() {
+            this.reset();
+            this.$replay.hide();
+            this.start();
+        },
+
+        reset: function() {
+            this.nextEventIndex = 0;
+            chart.series[0].update({
+                data: this.data.initial
+            });
+            chart.series[0].redraw();
+        },
+
+        initializeTable: function() {
+            this.eventTable = []; 
+            var tbody = $("#event-table").find("tbody")
+            for (var k = 0, length = this.data.new_count; k < length; k++) {
+                var row = $("<tr>");
+                var i = $("<td>").html(k).addClass('event-table-index'); 
+                var j = $("<td>").addClass('event-table-index'); 
+                var d = $("<td>").addClass('event-table-time'); 
+                var r = $("<td>").addClass('event-table-time'); 
+                var e = $("<td>").addClass('event-table-time'); 
+                row.append(i).append(j).append(d).append(r).append(e);
+                tbody.append(row); 
+                this.eventTable.push({
+                    i: i,
+                    j: j,
+                    d: d,
+                    r: r,
+                    e: e,
+                    row: row
+                });
+            }
+            window.tbody = tbody;
+        },
+
+        round: function(x) {
+            return Math.round(x * 10000) / 10000;
+        },
+
+        showEventInfo: function(e) {
+            var row = this.eventTable[e.i - data.stable_count];
+
+            error = this.round(100 * Math.abs(e.rtt - e.d) / e.rtt);
+            if (!error) {
+                debugger;
+            }
+            row.j.html(e.j); 
+            row.d.html(this.round(e.d)); 
+            row.r.html(this.round(e.rtt)); 
+            row.e.html(error); 
+
+            for(var i = 0, length = this.data.new_count; i < length; i++) {
+                var r = this.eventTable[i];
+                if (r == row) {
+                    r.row.addClass('event-table-row-active');
+                }
+                else {
+                    r.row.removeClass('event-table-row-active');
+                }
+            }
+
+        },
+
+        registerClickEvents: function() {
+            var self = this;
+            this.$play.click(function() {
+                self.start();
+            });
+            this.$pause.click(function() {
+                self.pause();
+            });
+        },
+
+        animateUpcomingEvents: function() {
+
+            var self = this;
+
+            var animateNextEvent = function() {
+                if (self.isPaused) {
+                    return;
+                }
+                if (self.nextEventIndex < self.length) {
+                    var e = self.data.events[self.nextEventIndex++]
+                    self.showEventInfo(e);
+                    self.animateEvent(e);
+                    setTimeout(animateNextEvent, 130);
+                }
+                else {
+                    self.$pause.hide();
+                    self.$replay.show();
+                }
+            }
+            animateNextEvent();
+        },
+
+        animateEvent: function(e) {
+            if(this.point_i) {
+                this.point_i.update({
+                    marker: {
+                        fillColor: '#ff7043',
+                        radius: 4
+                    }
+                })
+            }
+            if(this.point_j) {
+                this.point_j.update({
+                    marker: {
+                        fillColor: '#4982b9',
+                        radius: 4
+                    }
+                })
+            }
+
+            this.point_i = chart.series[0].data[e.i];
+            this.point_j = chart.series[0].data[e.j];
+
+            this.point_j.update({
+                marker: {
+                    fillColor: '#3f51b5',
+                    radius: 5
+                }
+            })
+
+            var x_i = e['x_i'];
+            this.point_i.update({
+                x: x_i[0],
+                y: x_i[1],
+                z: x_i[2],
+                marker: {
+                    fillColor: '#f44336',
+                    radius: 5
+                }
+            }, true, {
+                duration: 120,
+                easing: 'ease-out'
+            });
+            
+        }
+    }; 
+
+    Animation.prototype.constructor = Animation; 
+
+    window.a = new Animation(data);
 
     // Give the points a 3D feel by adding a radial gradient
     Highcharts.getOptions().colors = $.map(Highcharts.getOptions().colors, function (color) {
         return {
             radialGradient: {
-                cx: 0.4,
-                cy: 0.3,
-                r: 0.5
+                cx: 0.1,
+                cy: 0.1,
+                r: 0.1
             },
             stops: [
                 [0, color],
@@ -69,33 +223,33 @@ $(function () {
         },
         plotOptions: {
             scatter: {
-                width: 10,
-                height: 10,
-                depth: 10
+                width: 2,
+                height: 2,
+                depth: 2
             }
         },
         yAxis: {
-            min: -6,
-            max: 6,
+            min: data.y_min * 1.1,
+            max: data.y_max * 1.1,
             title: null
         },
         xAxis: {
-            min: -6,
-            max: 6,
+            min: data.x_min * 1.1,
+            max: data.y_max * 1.1,
             gridLineWidth: 1
         },
         zAxis: {
-            min: 0,
-            max: 10,
+            min: data.z_min,
+            max: data.z_max + 1,
             showFirstLabel: false
         },
         legend: {
             enabled: false
         },
         series: [{
-            name: 'Reading',
-            colorByPoint: true,
-            data: getInitialPositions()
+            name: 'Coordinates',
+            colorByPoint: false,
+            data: data.initial
         }]
     });
 
