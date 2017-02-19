c001d4fe174819bd7170874d80dafefb1435c2da
Pramod Lakshmanan <pramodliv1@gmail.com>
2017-02-20 02:46:58 +0530

    Visualize final case - distributed Vivaldi

--- visualization/src/2d-viz.js
+++ visualization/src/2d-viz.js
@@ -6,8 +6,9 @@ import animationControlsMixin from './animation-controls-mixin'
 
 const $ = window.$
 const anime = window.anime
+const events3 = window.events3
 
-const colors = ['#3f51b5', '#ffc107', '#ff5722', '#795548']
+const colors = ['#3f51b5', '#ffc107', '#ff5722', '#795548', '#00BCD4', '#ff9800']
 
 class TwoDViz {
   
@@ -15,7 +16,9 @@ class TwoDViz {
                width, height,
                minX, maxX,
                minY, maxY,
-               initialCoords, events, showForcesAndArrows}) {
+               initialCoords, stagedInitialCoords,
+               events, stagedEvents, eventsSampleFactor,
+               showForcesAndArrows, showStage}) {
     this.el = container;
     this.width = width
     this.height = height
@@ -25,22 +28,38 @@ class TwoDViz {
     this.maxY = maxY
     this.showForcesAndArrows = showForcesAndArrows
 
-    this.initialCoords = initialCoords.map(c => this.toSVGCoord(c))
+    this.stagedInitialCoords = stagedInitialCoords
+                               .map(coords => coords.map(c => this.toSVGCoord(c)))
+    this.circles = new Array(initialCoords.length)
 
     this.events = events.map(e => {
+
+      let forces = {}
+      let totalForce = []
+      let coords = []
+      if (this.showForcesAndArrows) {
+        forces = e.forces.map(f => {
+          return {from: f.from, vector: this.toSVGCoord(f.vector)}
+        })
+        totalForce = this.toSVGCoord(Vector.add(e.x_i, e.totalForce))
+        coords = e.coords.map(c => this.toSVGCoord(c))
+      }
+
       return {
         i: e.i,
         x_i: this.toSVGCoord(e.x_i),
         x_i_next: this.toSVGCoord(e.x_i_next),
-        totalForce: this.toSVGCoord(Vector.add(e.x_i, e.totalForce)),
-        forces: e.forces.map(f => {
-          return {from: f.from, vector: this.toSVGCoord(f.vector)}
-        }),
-        coords: e.coords.map(c => this.toSVGCoord(c))
+        totalForce,
+        forces,
+        coords,
+        stage: e.stage
       }
     })
     
+    this.eventsSampleFactor = eventsSampleFactor
     this.nextEventIndex = 0
+    this.currentStage = -1
+    this.coordsPerStage = stagedInitialCoords[0].length
     this.length = this.events.length
 
     for (let key of Object.keys(animationControlsMixin)) {
@@ -49,9 +68,14 @@ class TwoDViz {
 
     this.initControls(container)
 
+    this.showStage = showStage
+    if (showStage) {
+      this.renderStage()
+    }
     if (showForcesAndArrows) {
       this.renderLegend()
     }
+
   }
 
   renderLegend() {
@@ -71,20 +95,47 @@ class TwoDViz {
     })
   }
 
+  renderStage() {
+    this.stageView = $("<div>").html("1")
+    let parent = $(this.el).find('.anim-control')
+    parent.append(this.stageView)
+  }
+
+  updateStageView(stage) {
+    this.stageView.html(stage + 1)
+  }
+
   reset() {
     this.nextEventIndex = 0
+    this.currentStage = -1
     this.empty()
     this.render()
     return this
   }
 
+  getNextEvent() {
+    const event = this.events[this.nextEventIndex]
+    this.nextEventIndex += this.eventsSampleFactor
+    return event
+  }
+
   async play() {
-    await this.moveCoordsToInitialPositions()
-    while (this.nextEventIndex < this.length - 4) {
+    
+    while (this.nextEventIndex < (this.length - 4)) {
       if (this.isPaused) {
         return
       }
-      await this.next()
+      const event = this.getNextEvent()
+      if (event.stage !== this.currentStage) {
+        if (event.stage < this.currentStage) {
+          debugger
+        }
+        this.currentStage = event.stage
+        this.updateStageView(this.currentStage)
+        await this.moveCoordsToInitialPositions(this.currentStage)
+        
+      }
+      await this.next(event)
     }
     this.showReplayButton()
   }
@@ -93,21 +144,25 @@ class TwoDViz {
     this.isPaused = true
   }
 
-  async next() {
-    const event = this.events[this.nextEventIndex++]
-    const endPoints = event.forces.map(({from, vector}) => {
-      return [event.x_i, event.coords[from]]
-    })
+  async next(event) {
+    if (this.nextEventIndex % 100 === 0) {
+      console.info('Event: ', this.nextEventIndex)
+    }
     if (this.showForcesAndArrows) {
+      const endPoints = event.forces.map(({from, vector}) => {
+        return [event.x_i, event.coords[from]]
+      })
       await this.drawLines(endPoints, {cls: 'force', stroke: '#F44336', showArrow: true})
       await this.drawLines([[event.x_i, event.totalForce]], {cls: 'force', stroke: '#FFEB3B', showArrow: true})
     }
-    await this.drawLines([[event.x_i, event.x_i_next]], {cls: 'force-step', stroke: colors[event.i], showArrow: this.showForcesAndArrows})
+    // await this.drawLines([[event.x_i, event.x_i_next]], {cls: 'force-step', stroke: colors[event.i % 4], showArrow: this.showForcesAndArrows})
     await this.movePoint(event.i, event.x_i_next)
     
     return new Promise((resolve, _reject) => {
-      this.removeForces()
       setTimeout(() => {
+        if (this.showForcesAndArrows) {
+          this.removeForces()
+        }
         resolve(true)
       }, 10)
     })
@@ -124,14 +179,19 @@ class TwoDViz {
 
   empty() {
     this.svg.remove()
+    if (this.showStage) {
+      this.updateStageView(0)
+    }
   }
 
-  async moveCoordsToInitialPositions() {
-    this.circles = new Array(this.initialCoords.length)
+  async moveCoordsToInitialPositions(stage) {
+    let coords = this.stagedInitialCoords[stage]
     let promises = []
-    for (let i = 0; i < this.initialCoords.length; i++) {
+    let startIndex = stage * this.coordsPerStage
+    for (let j = 0; j < this.coordsPerStage; j++) {
+      let i = startIndex + j
       this.circles[i] = this.drawPointAt([0, 0], i)
-      let p = this.movePoint(i, this.initialCoords[i])
+      let p = this.movePoint(i, coords[j])
       promises.push(p)
       await this.delay(40)
     }
@@ -145,11 +205,13 @@ class TwoDViz {
   }
 
   movePoint(index, [cx, cy]) {
+    
     return new Promise((resolve, reject) => {
       let circle = this.circles[index]
       $(circle).animate(
         {cx, cy},
         {
+          duration: 40,
           step: function(v1) {$(this).attr('cx', v1)},
           complete: function() {
             resolve(circle)
@@ -176,7 +238,7 @@ class TwoDViz {
       cx,
       cy,
       r: 3,
-      fill: colors[index]
+      fill: colors[index % 4]
     })
     this.svg.appendChild(circle)
     return circle
@@ -294,39 +356,75 @@ let initialCoords1 = [
 let width = window.innerWidth * 0.7
 let height = window.innerHeight * 0.8
 
-let twoD1 = new TwoDViz({
-  container: document.getElementById('two-d-viz-1'),
-  width,
-  height,
-  minX: -0.2,
-  maxX: 10.2,
-  minY: -0.2,
-  maxY: 9.2,
-  initialCoords: initialCoords1,
-  events: events1.events,
-  showForcesAndArrows: true
+// let twoD1 = new TwoDViz({
+//   container: document.getElementById('two-d-viz-1'),
+//   width,
+//   height,
+//   minX: -0.2,
+//   maxX: 10.2,
+//   minY: -0.2,
+//   maxY: 9.2,
+//   initialCoords: initialCoords1,
+//   events: events1.events,
+//   eventsSampleFactor: 1,
+//   showForcesAndArrows: true,
+//   showStage: false,
+// })
+
+// twoD1.render()
+
+// let initialCoords2 = [
+//   [0, 0],
+//   [0, 0],
+//   [0, 0],
+//   [0, 0]
+// ]
+
+// let twoD2 = new TwoDViz({
+//   container: document.getElementById('two-d-viz-2'),
+//   width,
+//   height,
+//   minX: -4.3,
+//   maxX: 5.2,
+//   minY: -4,
+//   maxY: 4.8,
+//   initialCoords: initialCoords2,
+//   events: events2.events,
+//   eventsSampleFactor: 1,
+//   showForcesAndArrows: false,
+//   showStage: false,
+// })
+
+// twoD2.render()
+
+events3.initial_coords = events3.initial_coords.map(c => c.slice(0, 2))
+events3.staged_initial_coords = events3.staged_initial_coords.map(coords => coords.map(c => c.slice(0, 2)))
+events3.events = events3.events.map(e => {
+  e.forces = []
+  e.totalForce = [0, 0]
+  e.x_i = e.x_i.slice(0, 2)
+  e.x_i_next = e.x_i_next.slice(0, 2)
+  e.coords = []
+  return e
 })
 
-twoD1.render()
-
-let initialCoords2 = [
-  [0, 0],
-  [0, 0],
-  [0, 0],
-  [0, 0]
-]
-
-let twoD2 = new TwoDViz({
-  container: document.getElementById('two-d-viz-2'),
+let twoD3 = new TwoDViz({
+  container: document.getElementById('two-d-viz-3'),
   width,
   height,
-  minX: -4.3,
-  maxX: 5.2,
-  minY: -4,
-  maxY: 4.8,
-  initialCoords: initialCoords2,
-  events: events2.events,
-  showForcesAndArrows: false
+  minX: events3.min_x,
+  maxX: events3.max_x,
+  minY: events3.min_y,
+  maxY: events3.max_y,
+  initialCoords: events3.initial_coords,
+  stagedInitialCoords: events3.staged_initial_coords,
+  events: events3.events,
+  stagedEvents: events3.staged_events,
+  eventsSampleFactor: 3,
+  showForcesAndArrows: false,
+  showStage: true
 })
 
-twoD2.render()
+twoD3.render()
+window.t = twoD3
+
