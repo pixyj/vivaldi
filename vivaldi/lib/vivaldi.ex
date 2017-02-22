1bf560a226288e33df0a36630ea2a15c8cf1719a
Pramod Lakshmanan <pramodliv1@gmail.com>
2017-02-23 03:40:46 +0530

    Remove unused code.

--- vivaldi/lib/vivaldi.ex
+++ vivaldi/lib/vivaldi.ex
@@ -10,26 +10,16 @@ defmodule Vivaldi do
     node_id = options[:nodeid] |> String.to_atom()
     node_name = options[:nodename] |> String.to_atom()
     cookie = options[:cookie] |> String.to_atom()
-    controller_name = options[:controllername] |> String.to_atom()
     
     {:ok, _pid} = Node.start(node_name)
     Node.set_cookie(cookie)
 
-    Logger.info "#{node_id} - Started node #{Node.self}"
+    Logger.info "#{node_id} - #{Node.self} is up!. Waiting for controller to connect..."
 
-    case Node.connect(controller_name) do 
-      true ->
-        Supervisor.start_link(node_id)
+    Supervisor.start_link(node_id)
 
-        # I couldn't get distillery to work. 
-        # So here's a hack to keep the application running 
-        # until we collect enough data for the experiment. 
-        :timer.sleep(1000 * 86400)
-      _ ->
-        Logger.error "Could not connect to controller. Exiting..."
-    end
-
-
+    # This is a hack. Don't exit since controller will connect soon!
+    :timer.sleep(1000 * 86400)
   end
   
 end
